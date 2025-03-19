79e57456dd2c7b5ea5d7dbcdb9b84faa59e813a4
rasha <aysharasha258@gmail.com>
2025-03-19 16:12:50 +0530

    added cors

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ moongose
 })
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://carbon-frontend-two.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.use('/',carbonRoute);
 
